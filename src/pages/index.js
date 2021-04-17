2987a7bbba9807f388480f032cc569ba711a5b71
Paolo <paolo.arrata@gmail.com>
2021-04-17 17:04:09 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,28 @@
+import Layout from '../components/Layout';
+import getComics from '../data/data.js'
+import Photo from '../components/Photo';
+import fetch from 'node-fetch';
+
+const Index = (props) => (
+    <Layout>
+        {
+            props.comics.map((comic, key) => <Photo id={key} key={key} data={comic} />)
+        }
+    </Layout>
+);
+
+export const getStaticProps = async ({ }) => {
+    // Would fetch data
+    // Call an external API endpoint to get posts.
+    // const res = await fetch('https://wea4jmuke6.execute-api.us-east-2.amazonaws.com/Prod/api/v1/comics');
+    // const comics = await res.json();
+
+    return {
+        props: {
+            comics: getComics(),
+            // comics: comics.data.results
+        }
+    }
+};
+
+export default Index
